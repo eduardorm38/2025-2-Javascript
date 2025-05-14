2fc46fc5b98b252db8dfa9d225f3cc15fb3a1994
eduardorm38 <erm@cse.com.mx>
2025-05-13 21:23:01 -0600

    semana 2 clase 2

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -3,8 +3,23 @@
 
 const {ask}=require('../helper/input');
 
-async function main(){
-    let limite= Number(await ask('Hast'));
-    let i = 0;
-while ()
+function main(){
+//    let limite= Number(('Hast'));
+   // let i = 0;
+for (let i > 0 ; i <= 100 ; i ++){
+    if (i%2===0){
+    console. log(i);
+   
 }
+}
+}
+main();
+
+// for (let j = 1; j <= 5; j++) {
+//    if (j === cajon) {
+//      console.log("En el cajón hay: ", frutas[j - 1]);
+//    }
+//  }
+//}
+
+//main();
